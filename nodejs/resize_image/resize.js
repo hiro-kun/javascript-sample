b080e2698cd1d484f8b09df7681e069399b7ca0b
ono-hiroshi <hiroshi_ono_1989@yahoo.co.jp>
2018-01-11 16:05:27 +0900

    fix class化

--- nodejs/resize_image/resize.js
+++ nodejs/resize_image/resize.js
@@ -7,73 +7,63 @@ const IMG_WIDTH = 500;
 
 const jimp = require('jimp');
 
-/**
- * リサイズ対象画像ファイルパスを取得
- *
- * @return {array} リサイズ対象ファイル
- * @throws {object} エラーオブジェクト
-*/
-function retrieveTargetFilePathList()
-{
-    return new Promise((resolve, reject) => {
-        const fs = require('fs');
 
-        fs.readdir(IMG_DIR, (err, files) => {
 
-            if (err) reject(err);
+const resizeImage = class {
 
-            const target_files = [];
+    static retrieveTargetFilePathList()
+    {
+        return new Promise((resolve, reject) => {
+            const fs = require('fs');
 
-            files.forEach(file => {
+            fs.readdir(IMG_DIR, (err, files) => {
 
-                if (/out/.test(file)) {
-                    return;
-                }
+                if (err) reject(err);
 
-                // jpg以外はスキップ
-                if (!file.match(/jpg/)) {
-                    return;
-                }
+                const target_files = [];
 
-                target_files.push(file);
+                files.forEach(file => {
+
+                    if (/out/.test(file)) {
+                        return;
+                    }
+
+                    // jpg以外はスキップ
+                    if (!file.match(/jpg/)) {
+                        return;
+                    }
+
+                    target_files.push(file);
+                });
+
+                resolve(target_files);
             });
-
-            resolve(target_files);
         });
-    });
+    }
+
+    static imageResize(file)
+    {
+        const path = require('path');
+        const image_write_path =  `${IMG_OUTPUT_DIR}${path.basename(file, path.extname(file))}_out.jpg`;
+
+        jimp.read(IMG_DIR + file).then(lenna => {
+            lenna.resize(IMG_HEIGHT, IMG_WIDTH)
+                .quality(100)
+                .greyscale()
+                .write(image_write_path);
+        }).then(() => {
+            console.log(image_write_path + ' is Resized.');
+        }).catch(err => {
+            console.log('Image resize error.');
+            console.log(err);
+        });
+    }
 }
 
-/**
- * 画像リサイズ
- *
- * @param {array} リサイズ対象ファイル
- * @throws {string} エラーメッセージ
-*/
-function imageResize(file)
-{
-    const path = require('path');
-    const image_write_path =  `${IMG_OUTPUT_DIR}${path.basename(file, path.extname(file))}_out.jpg`;
-
-    jimp.read(IMG_DIR + file).then(lenna => {
-        lenna.resize(IMG_HEIGHT, IMG_WIDTH)
-            .quality(100)
-            .greyscale()
-            .write(image_write_path);
-    }).then(() => {
-        console.log(image_write_path + ' is Resized.');
-    }).catch(err => {
-        console.log('Image resize error.');
-        console.log(err);
-    });
-}
-
-
-
-
-retrieveTargetFilePathList()
+resizeImage.retrieveTargetFilePathList()
     .then((files) => {
         Promise.all(files.map(file => {
-            imageResize(file);
+            resizeImage.imageResize(file);
         }));
     }).catch((error) => {
         console.log(error);
