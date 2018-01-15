546f501e7e9f3f4acdc928b68e785d3b86fbcbc0
ono-hiroshi <hiroshi_ono_1989@yahoo.co.jp>
2018-01-15 23:11:23 +0900

    fix esLintの結果を修正

--- nodejs/resize_image/index.js
+++ nodejs/resize_image/index.js
@@ -1,15 +1,16 @@
 const ResizeImage = require('./lib/resize.js');
+
 const resizeImage = new ResizeImage('img/', 'out/');
 
-const main = async() => {
-    const resizeTargetFileList = await resizeImage.retrieveTargetFilePathList();
+const main = async () => {
+  const resizeTargetFileList = await resizeImage.retrieveTargetFilePathList();
 
-    for (let i = 0; i < resizeTargetFileList.length; i += 1) {
-        const result = await resizeImage.imageResize(resizeTargetFileList[i], 500, 500);
-        console.log(result);
-    }
+  for (let i = 0; i < resizeTargetFileList.length; i += 1) {
+    const result = await resizeImage.imageResize(resizeTargetFileList[i], 500, 500);
+    console.log(result);
+  }
 
-    console.log('Process all finish.')
+  console.log('Process all finish.');
 };
 
 main();
--- nodejs/resize_image/lib/resize.js
+++ nodejs/resize_image/lib/resize.js
@@ -1,61 +1,54 @@
-'use strict';
 
-module.exports = class ResizeImage {
+const fs = require('fs');
+const path = require('path');
+const jimp = require('jimp');
 
-    constructor(img_dir, img_output_dir) {
-        this.img_dir = 'img/';
-        this.output_dir = 'out/';
-    }
+module.exports = class {
+  constructor(imgDir, outputDir) {
+    this.imgDir = imgDir;
+    this.outputDir = outputDir;
+  }
 
-    retrieveTargetFilePathList()
-    {
-        return new Promise((resolve, reject) => {
-            const fs = require('fs');
+  retrieveTargetFilePathList() {
+    return new Promise((resolve, reject) => {
+      fs.readdir(this.imgDir, (err, files) => {
+        if (err) reject(err);
 
-            fs.readdir(this.img_dir, (err, files) => {
+        const targetFiles = [];
 
-                if (err) reject(err);
+        files.forEach((file) => {
+          if (/out/.test(file)) {
+            return;
+          }
 
-                const target_files = [];
+          // jpg以外はスキップ
+          if (!file.match(/jpg/)) {
+            return;
+          }
 
-                files.forEach(file => {
-
-                    if (/out/.test(file)) {
-                        return;
-                    }
-
-                    // jpg以外はスキップ
-                    if (!file.match(/jpg/)) {
-                        return;
-                    }
-
-                    target_files.push(file);
-                });
-
-                resolve(target_files);
-            });
+          targetFiles.push(file);
         });
-    }
 
-    imageResize(file, height, width)
-    {
-        return new Promise((resolve, reject) => {
-            const path = require('path');
-            const jimp = require('jimp');
-            const image_write_path = `${this.output_dir}${path.basename(file, path.extname(file))}_out.jpg`;
+        resolve(targetFiles);
+      });
+    });
+  }
 
-            jimp.read(this.img_dir + file).then(lenna => {
-                lenna.resize(height, width)
-                    .quality(100)
-                    .greyscale()
-                    .write(image_write_path);
+  imageResize(file, height, width) {
+    return new Promise((resolve, reject) => {
+      const imageWritePath = `${this.outputDir}${path.basename(file, path.extname(file))}_out.jpg`;
 
-                resolve(image_write_path + ' is Resized.');
+      jimp.read(this.imgDir + file).then((lenna) => {
+        lenna.resize(height, width)
+          .quality(100)
+          .greyscale()
+          .write(imageWritePath);
 
-            }).catch(err => {
-                console.log(err);
-                reject(err);
-            });
-        });
-    }
+        resolve(`${imageWritePath} is Resized.`);
+      }).catch((err) => {
+        console.log(err);
+        reject(err);
+      });
+    });
+  }
 };
