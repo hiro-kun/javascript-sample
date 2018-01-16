1546df014a536ec37c5d8fce31ef1104fa0945f6
ono-hiroshi <hiroshi_ono_1989@yahoo.co.jp>
2018-01-16 23:44:49 +0900

    fix ESLintの修正を適用

--- promise/promise.js
+++ promise/promise.js
@@ -1,25 +1,19 @@
-'use strict';
+const getMessage = (name) => new Promise((resolve, reject) => {
+  if (typeof name !== 'string') {
+    reject(new Error('Input parameter must be string.'));
+  }
 
-function getMessage(name) {
-    return new Promise((resolve, reject) => {
+  const messageTemplate = 'My name is ';
 
-        if (typeof name !== 'string') {
-            reject('Input parameter must be string.');
-        }
-
-        const messageTemplate = 'My name is ';
-
-        resolve(messageTemplate + name);
-
-    });
-}
+  resolve(messageTemplate + name);
+});
 
 const name = 'hoge huga';
 // let name = 111;
 
 getMessage(name)
-    .then((message) => {
-        console.log(message);
-    }).catch((errorMessage) => {
-        console.log(errorMessage);
-    });
+  .then((message) => {
+    console.log(message);
+  }).catch((errorMessage) => {
+    console.log(errorMessage);
+  });
