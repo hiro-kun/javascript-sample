14f35a0caf70ca56a7e64ed83ded7901964a81ef
ono-hiroshi <hiroshi_ono_1989@yahoo.co.jp>
2018-01-16 23:17:38 +0900

    fix ESLintの修正を適用

--- promise/promise2.js
+++ promise/promise2.js
@@ -1,15 +1,13 @@
-'use strict';
-
-const promiseSample = new Promise((resolve, reject) => {
-    window.setTimeout(() => {
-        resolve('OK');
-    }, 500);
+const promiseSample = new Promise((resolve) => {
+  setTimeout(() => {
+    resolve('OK');
+  }, 500);
 });
 
 promiseSample
-    .then((result) => {
-        console.log(result);
-    })
-    .catch((error) => {
-        console.log(error);
-    });
+  .then((result) => {
+    console.log(result);
+  })
+  .catch((error) => {
+    console.log(error);
+  });
